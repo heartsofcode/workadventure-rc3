98d7e7b538ada00687c1e3d5520e65a6f486bdfe
wunderfisch <34567581+wunderfisch@users.noreply.github.com>
2021-12-14 00:10:48 +0100

    Add files via upload

---
+++ dome.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="dome" tilewidth="32" tileheight="32" tilecount="80" columns="10">
+ <image source="../HoC/tiles/dome-gold.png" width="320" height="256"/>
+</tileset>
