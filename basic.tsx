bbf1d6fc54bff68ec7253d8050c04986bb210d3c
lelilia <lelilia@posteo.de>
2021-12-25 21:26:14 +0100

    silent

---
+++ basic.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="basic" tilewidth="32" tileheight="32" tilecount="1200" columns="30">
+ <image source="../rc3-2021-mullana-utility.png" width="960" height="1280"/>
+ <tile id="1051">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="1087">
+  <properties>
+   <property name="jitsiRoom" value="fuck_room"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ functions.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="functions" tilewidth="32" tileheight="32" tilecount="9" columns="9">
+ <image source="mapUtilities.png" width="288" height="32"/>
+ <tile id="3">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="4">
+  <properties>
+   <property name="silent" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
