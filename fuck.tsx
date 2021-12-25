5511004facee98b9e6514a76511bf491ff0a9d96
lelilia <lelilia@posteo.de>
2021-12-25 18:10:53 +0100

    animiertes fuck

---
+++ fuck.tsx
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="fuck" tilewidth="900" tileheight="600" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="900" height="600" source="../logo-fuck-final-dark.png"/>
+  <animation>
+   <frame tileid="1" duration="600"/>
+   <frame tileid="0" duration="130"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="900" height="600" source="../logo-fuck-final.png"/>
+ </tile>
+</tileset>
