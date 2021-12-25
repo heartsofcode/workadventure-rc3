3b734f3e2176b0fc7e6749aad0e9c9cf62aa16a1
lelilia <lelilia@posteo.de>
2021-12-25 20:31:31 +0100

    status quo

---
+++ antrieb_abc.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="antrieb_abc" tilewidth="1394" tileheight="903" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="1394" height="903" source="../antrieb_a.png"/>
+  <animation>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="0" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <image width="1394" height="903" source="../antrieb_b.png"/>
+ </tile>
+ <tile id="2">
+  <image width="1394" height="903" source="../antrieb_c.png"/>
+ </tile>
+</tileset>
