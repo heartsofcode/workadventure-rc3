f473c61213a77b8abfacee60a708461711df1cab
miriamino <29210881+miriamino@users.noreply.github.com>
2021-12-25 21:12:25 +0100

    cleanup

--- fuck.tsx
+++ fuck.tsx
@@ -2,14 +2,14 @@
 <tileset version="1.5" tiledversion="1.7.2" name="fuck" tilewidth="900" tileheight="600" tilecount="2" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
-  <image width="900" height="600" source="../logo-fuck-final-dark.png"/>
+  <image width="900" height="600" source="logo-fuck-final-dark.png"/>
   <animation>
    <frame tileid="1" duration="600"/>
    <frame tileid="0" duration="130"/>
   </animation>
  </tile>
  <tile id="1">
-  <image width="900" height="600" source="../logo-fuck-final.png"/>
+  <image width="900" height="600" source="logo-fuck-final.png"/>
   <animation>
    <frame tileid="0" duration="2000"/>
    <frame tileid="1" duration="200"/>
