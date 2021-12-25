f79d518adc606cd538d7162dc82156cb456f29c6
miriamino <29210881+miriamino@users.noreply.github.com>
2021-12-25 14:10:29 +0100

    mehr pixels

---
+++ circuit_rooms.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="circuit_rooms" tilewidth="32" tileheight="32" tilecount="4575" columns="75">
+ <image source="circuit_rooms.png" width="2415" height="1976"/>
+</tileset>
---
+++ heartship.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="heartship" tilewidth="32" tileheight="32" tilecount="4575" columns="75">
+ <image source="heartship.png" width="2415" height="1976"/>
+</tileset>
