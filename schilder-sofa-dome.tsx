4576bea5a577a96d214f0511aa0339538a111c18
wunderfisch <34567581+wunderfisch@users.noreply.github.com>
2021-12-27 00:45:30 +0100

    changes_211226

---
+++ schilder-sofa-dome.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="schilder-sofa-dome" tilewidth="32" tileheight="32" tilecount="400" columns="20">
+ <image source="schilder-sofa-dome.png" width="660" height="660"/>
+</tileset>
